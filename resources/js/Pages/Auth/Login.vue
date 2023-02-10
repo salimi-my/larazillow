fd1aba5761c2fc4b60af0ce2a8e1150ec272f4fb
Mohamad Salimi <contact@salimi.my>
2023-02-10 22:28:21 +0800

    Add authentication logic

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -2,13 +2,14 @@
   <form @submit.prevent="login">
     <div class="w-1/2 mx-auto">
       <div>
-        <label for="email">E-mail (username)</label>
+        <label for="email">E-mail</label>
         <input type="text" v-model="form.email" id="email" class="input" />
-        <div class="input-error">Potential errors</div>
+        <div class="input-error" v-if="form.errors.email">{{ form.errors.email }}</div>
       </div>
       <div class="mt-4">
         <label for="password">Password</label>
         <input type="password" v-model="form.password" id="password" class="input" />
+        <div class="input-error" v-if="form.errors.password">{{ form.errors.password }}</div>
       </div>
       <div class="mt-4">
         <button class="btn-primary w-full" type="submit">Login</button>
