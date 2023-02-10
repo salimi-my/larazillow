3f1b521845390e5a0e3f912dbc7e8780012ab7a4
Mohamad Salimi <contact@salimi.my>
2023-02-10 22:47:35 +0800

    Add logout

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -8,10 +8,17 @@
         <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
           <Link :href="route('listing.index')">LaraZillow</Link>
         </div>
-        <div>
+        <div v-if="user" class="flex items-center gap-4">
+          <div class="text-sm text-gray-500">{{ user.name }}</div>
           <Link :href="route('listing.create')" class="btn-primary">+ New
           Listing
           </Link>
+          <div>
+            <Link :href="route('logout')" method="delete" as="button">Logout</Link>
+          </div>
+        </div>
+        <div v-else>
+          <Link :href="route('login')">Sign-In</Link>
         </div>
       </nav>
     </div>
@@ -30,5 +37,11 @@
 import { Link, usePage } from '@inertiajs/vue3'
 import { computed } from 'vue'
 
-const flashSuccess = computed(() => usePage().props.flash.success)
+const flashSuccess = computed(
+  () => usePage().props.flash.success,
+)
+
+const user = computed(
+  () => usePage().props.user,
+)
 </script>
