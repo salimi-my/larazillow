920e381ee2f2b231bd7a5445dd6ea9d868289a01
Mohamad Salimi <contact@salimi.my>
2023-02-18 17:22:58 +0800

    Add pagination

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -21,7 +21,7 @@ class ListingController extends Controller
     public function index()
     {
         return inertia('Listing/Index', [
-            'listings' => Listing::all()
+            'listings' => Listing::orderByDesc('created_at')->paginate(10)
         ]);
     }
 
