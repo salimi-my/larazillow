5dcb0c586106e03fd52cc7440b75335d8174d51b
Mohamad Salimi <contact@salimi.my>
2023-02-19 19:25:23 +0800

    Delete filter markup

--- resources/js/Pages/Realtor/Index.vue
+++ resources/js/Pages/Realtor/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <h1 class="text-3xl mb-4">Your Listings</h1>
-  <section class="mb-4">
-    Filters
+  <section class="mb-8">
+    <RealtorFilters />
   </section>
   <section class="grid grid-cols-1 lg:grid-cols-2 gap-2">
     <Box v-for="listing in listings" :key="listing.id">
@@ -32,6 +32,7 @@ import ListingSpace from '@/Components/ListingSpace.vue'
 import Price from '@/Components/Price.vue'
 import Box from '@/Components/UI/Box.vue'
 import { Link } from '@inertiajs/vue3'
+import RealtorFilters from '@/Pages/Realtor/Index/Components/RealtorFilters.vue'
 
 defineProps({
   listings: Array,
