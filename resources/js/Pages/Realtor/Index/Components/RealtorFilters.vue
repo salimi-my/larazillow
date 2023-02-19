6f534fefe46cd840d626f9d61ad605abb374d195
Mohamad Salimi <contact@salimi.my>
2023-02-19 20:27:06 +0800

    watcher

--- resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
+++ resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
@@ -2,10 +2,28 @@
   <form>
     <div class="mb-4 mt-4 flex flex-wrap gap-2">
       <div class="flex flex-nowrap items-center gap-2">
-        <input id="deleted" type="checkbox"
+        <input id="deleted" v-model="filterForm.deleted" type="checkbox"
           class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
         <label for="deleted">Deleted</label>
       </div>
     </div>
   </form>>
 </template>
+
+<script setup>
+import { router } from '@inertiajs/core'
+import { reactive, watch } from 'vue'
+
+const filterForm = reactive({
+  deleted: false,
+})
+
+// reactive or ref or computed
+watch(filterForm, () => {
+  router.get(
+    route('realtor.listing.index'),
+    filterForm,
+    { preserveState: true, preserveScroll: true },
+  )
+})
+</script>
