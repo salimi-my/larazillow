4620f80a4dd3d9aeb6efea37352083eaa1649712
Mohamad Salimi <contact@salimi.my>
2023-02-26 20:01:52 +0800

    Upload progress bar

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -2,10 +2,18 @@
   <Box>
     <template #header>Upload New Images</template>
     <form @submit.prevent="upload" enctype="multipart/form-data">
-      <section class="flex items-center gap-2 my-4">
-        <input
-          class="border rounded-md file:px-4 file:py-2 border-gray-200 dark:border-gray-700 file:text-gray-700 file:dark:text-gray-400 file:border-0 file:bg-gray-100 file:dark:bg-gray-800 file:font-medium file:hover:bg-gray-200 file:dark:hover:bg-gray-700 file:hover:cursor-pointer file:mr-4"
-          type="file" multiple @input="addFiles" />
+      <section class="flex items-start gap-2 my-4">
+        <div class="flex flex-col">
+          <input
+            class="border rounded-md file:px-4 file:py-2 border-gray-200 dark:border-gray-700 file:text-gray-700 file:dark:text-gray-400 file:border-0 file:bg-gray-100 file:dark:bg-gray-800 file:font-medium file:hover:bg-gray-200 file:dark:hover:bg-gray-700 file:hover:cursor-pointer file:mr-4"
+            type="file" multiple @input="addFiles" />
+          <div v-if="form.progress" class="w-full bg-neutral-200 dark:bg-neutral-600 rounded-lg mt-4">
+            <div class="bg-indigo-600 p-0.5 text-center text-xs font-medium leading-none text-white rounded-lg"
+              :style="{ width: form.progress.percentage + '%' }">
+              {{ form.progress.percentage }}%
+            </div>
+          </div>
+        </div>
         <button type="submit" class="btn-outline disabled:opacity-25 disabled:cursor-not-allowed"
           :disabled="!canUpload">Upload</button>
         <button type="reset" @click="reset" class="btn-outline">Reset</button>
