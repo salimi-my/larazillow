243c122c386fe50e3bf079f5aaee3beeb87aecc5
Mohamad Salimi <contact@salimi.my>
2023-03-02 23:59:32 +0800

    ListingOfferController & update route

---
+++ app/Http/Controllers/ListingOfferController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ListingOfferController extends Controller
+{
+    public function store(Listing $listing, Request $request)
+    {
+    }
+}
